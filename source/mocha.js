716897157cf581691e67f7edb13d9c7187f002b3
Dr. Safi <sfmissive@gmail.com>
2018-09-21 06:28:43 +0300

    ✒ Watch files and re-run tests.

--- source/mocha.js
+++ source/mocha.js
@@ -1,68 +1,106 @@
 /**
  * Programmatic rest runner https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically
  * //TODO: Work with JSDom module for frontend testing using nodejs.
- * // TODO: live reload - watch file for changes and re run the tests
  */ 
 import path from 'path'
+import chokidar from 'chokidar'
 import Mocha from 'mocha'
-const mocha = new Mocha(); // Instantiate a Mocha instance.
 import { listFileRecursively } from './utility/listFileRecursively.js'
-const extensionName = '.test.js'
-let testPath = process.argv.slice(2)[0] // get first argument variable (either file path or directory path) 
-console.log(process.argv)
-/* List all files in a directory recursively */
-console.log(`• Searching for ${extensionName} extension files, in path ${testPath}.`)
 
-// Add each .js file to the mocha instance
-if(testPath.endsWith(extensionName)) { // single test file path
-    mocha.addFile(testPath)
-} else { // treat path as directory
-    listFileRecursively({directory: testPath})
+function invoke({ 
+    testPath, 
+    extensionName = '.test.js',
+}) {
+    /* List all files in a directory recursively */
+    console.log(`• Searching for ${extensionName} extension files, in path ${testPath}.`)
+    let fileArray = listFileRecursively({directory: testPath})
         .filter(file => {
             // Only keep the .test.js files
             return file.name.substr(-extensionName.length) === extensionName;
         })
-        .forEach((file) => {
+        .reduce((accumulator, currentValue) => {
+            accumulator.push(currentValue.path)
+            return accumulator
+        }, [])
+
+    runMocha({ testPath, extensionName, fileArray }) // initial run
+
+    let watcher = chokidar.watch(fileArray, {
+        ignored: new RegExp(/node_modules/),
+        usePolling: false
+    })
+    watcher
+        .on('ready', path => {
             console.group('List of paths:')
-            console.log(file)
+            console.log(watcher.getWatched())
+            console.log('\n\n')
             console.groupEnd()
-            mocha.addFile(file.path)
         })
-
+        .on('add', path => {
+            console.log(`File ${path} has been added`)
+        })
+        .on('change', path => {
+            console.log(`File ${path} has been changed`)
+            runMocha({ testPath, extensionName, fileArray })
+        })
+        .on('unlink', path => console.log(`File ${path} has been removed`))
 }
 
-// Run tests.
-try {
-    mocha.run(error => {
-            // exit with non-zero status if there were failures
-            if(error) { 
-                throw error
-            }
-            process.on('exit', () => {
-                process.exit(error)
+function runMocha({ 
+    mocha = new Mocha(), // Instantiate a Mocha instance.
+    testPath,
+    extensionName,
+    fileArray
+}) {
+    mocha.suite.on('require', function (global, file) { delete require.cache[file] }) // Fixes issue ❗ - Allow running multiple instances of `mocha` by reseting require.cache https://github.com/mochajs/mocha/issues/995
+
+    // Add each .test.js file to the mocha instance
+    if(testPath.endsWith(extensionName)) { // single test file path
+        mocha.addFile(testPath)
+    } else { // treat path as directory
+        fileArray.forEach((file) => {
+                mocha.addFile(file)
             })
-            process.exit()
-        })
-        // .on('test', function(test) {
-        //     console.log('Test started: '+test.title);
-        // })
-        // .on('test end', function(test) {
-        //     console.log('Test done: '+test.title);
-        // })
-        // .on('pass', function(test) {
-        //     console.log('Test passed');
-        //     console.log(test);
-        // })
-        // .on('fail', function(test, err) {
-        //     console.log('Test fail');
-        //     console.log(test);
-        //     console.log(err);
-        // })
-        // .on('end', function() {
-        //     console.log('All done');
-        // })
-
+    }
     
-} catch (error) {    
-    throw error
+    // Run tests.
+    try {
+        mocha.run(error => {
+                // exit with non-zero status if there were failures
+                if(error) { 
+                    throw error
+                }
+                // process.exit()
+            })
+            // .on('test', function(test) {
+            //     console.log('Test started: '+test.title);
+            // })
+            // .on('test end', function(test) {
+            //     console.log('Test done: '+test.title);
+            // })
+            // .on('pass', function(test) {
+            //     console.log('Test passed');
+            //     console.log(test);
+            // })
+            // .on('fail', function(test, err) {
+            //     console.log('Test fail');
+            //     console.log(test);
+            //     console.log(err);
+            // })
+            // .on('end', function() {
+            //     console.log('All done');
+            // })
+
+        
+    } catch (error) {    
+        throw error
+    }    
 }
+
+function cliInterface() {
+    // console.log(process.argv)
+    let testPath = process.argv.slice(2)[0] // get first argument variable (either file path or directory path) 
+    invoke({ testPath })
+}
+
+cliInterface()
--- source/utility/listFileRecursively.js
+++ source/utility/listFileRecursively.js
@@ -1,6 +1,7 @@
 import filesystem from 'fs'
 import path from 'path'
 
+// returns all files in nested directory.
 let listFileRecursively = ({directory}) => {
     let results = []
     let list = filesystem.readdirSync(directory)
@@ -8,7 +9,8 @@ let listFileRecursively = ({directory}) => {
         let filepath = path.join(directory, filename)
         let stat = filesystem.statSync(filepath)
         if (stat && stat.isDirectory()) results = results.concat(listFileRecursively({directory: filepath}))
-        else results.push({ name: filename, path: filepath })
+        else results.push({ name: filename, path: filepath }) // create object
+
     })
     return results
 }

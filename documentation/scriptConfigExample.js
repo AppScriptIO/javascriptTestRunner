cadf5c0a50a780830e0d5d56719dee15b44dd145
Dr. Safi <sfmissive@gmail.com>
2019-02-02 17:00:01 +0200

    📃 Usage example with `scriptManager`

---
+++ documentation/scriptConfigExample.js
@@ -0,0 +1,12 @@
+// configuration example for usage with `scriptManager` module,
+let scriptConfig = {
+    key: 'test',
+    type: 'module',
+    methodName: 'runTest',
+    adapterFunction: ({ callback, args }) => {
+        // change api to specific script parameter name
+        args[0].targetProject = args[0].api.project
+        return () => callback(...args) // specific interface of the callback
+    },
+    path: `${resolvedModule.javascriptTestRunner}/source/entrypoint/module/transpilation.entrypoint.js`,
+}

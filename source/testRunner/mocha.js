e0267db998e8f9f6f034a76e6d673a52cb40d8cb
Dr. Safi <sfmissive@gmail.com>
2019-02-04 04:28:54 +0200

    ⚗ Move functionality to utility folder.

--- source/testRunner/mocha.js
+++ source/testRunner/mocha.js
@@ -1,3 +1,5 @@
+import { invalidateRequiredModule } from './utility/invalidateRequiredModule.js'
+
 /**
  * Mocha -Programmatic rest runner https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically
  */ 
@@ -60,18 +62,3 @@ export function runMocha({
 }
 
 
-/**
- * hakish way to invalidate all required modules caches in the Node process, which will allow for changes in .test.js or .js files to propagate in the re-started mocha tests.  
- * Where test files are invalidated by mocha's 'require' hook and other files (.js) are invalidated with 'beforeAll' hook. (Could use only 'beforeAll' but this is not a straight forward method anyway.)
- **/ 
-function invalidateRequiredModule({ mochaInstance, fileArray }) {
-    mochaInstance.suite.on('require', function (global, file) { // invalidate only test files, not the child files required in test files. i.e. ISSUE: any file required inside test files won't be discarded and reloaded.
-        delete require.cache[file]
-    }) // Fixes issue ❗ - Allow running multiple instances of `mocha` by reseting require.cache https://github.com/mochajs/mocha/issues/995
-    
-    // invalidate js files as well.
-    console.log(`\x1b[2m%s\x1b[0m`, `• [Cleanning up require in current node process] Invalidating require cache of ${fileArray.length} file + test files.\n`)
-    fileArray.forEach((file) => {
-        delete require.cache[file]
-    })
-}

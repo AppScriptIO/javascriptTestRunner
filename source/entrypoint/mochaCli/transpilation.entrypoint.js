1cf721eabd8380174a3cbd02602888f71b9a0a2b
Safi <sfmissive@gmail.com>
2019-04-05 11:24:38 +0300

    ✒ Add parameter for outputing transpiled files to console/filesystem.

--- source/entrypoint/mochaCli/transpilation.entrypoint.js
+++ source/entrypoint/mochaCli/transpilation.entrypoint.js
@@ -1,7 +1,7 @@
 
 /* Entrypoint chain */
 // • Transpilation (babelJSCompiler)
-require('@dependency/javascriptTranspilation')({ babelConfigurationFile: 'serverRuntime.BabelConfig.js' })
+require('@dependency/javascriptTranspilation')({ babelConfigurationFile: 'serverRuntime.BabelConfig.js', outputTranspilation: false })
 
 // • Run
 module.exports = require('./')

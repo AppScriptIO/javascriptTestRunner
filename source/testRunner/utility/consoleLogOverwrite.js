f63e17587aec351fa895573724152025c7b91ae5
Safi <sfmissive@gmail.com>
2019-05-10 05:28:30 +0300

    ✒ Indent subprocess with random color.

--- source/testRunner/utility/consoleLogOverwrite.js
+++ source/testRunner/utility/consoleLogOverwrite.js
@@ -2,9 +2,9 @@
 export function consoleLogOverwrite() {
   console.log = new Proxy(console.log, {
     apply(target, thisArg, argumentsList) {
-      const blueColor = `\u001b[44m`,
-        resetColor = `\u001b[0m` // https://misc.flogisoft.com/bash/tip_colors_and_formatting
-      const prefix = `${blueColor}  ${resetColor}`
+      const color = [`\u001b[44m`, '\u001B[42m', '\u001B[35m', '\u001B[36m', '\u001B[37m', '\u001B[41m'],
+        resetColor = `\u001b[0m` // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println/27788295
+      const prefix = `${Math.floor(Math.random() * color.length)}  ${resetColor}`
       process.stdout.write(prefix)
       return Reflect.apply(target, thisArg, argumentsList)
     },
